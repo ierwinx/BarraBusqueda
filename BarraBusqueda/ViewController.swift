d55edce3a3342241320b4da49ca449ea7dca1e6d
Erwin Luz Leon <eluz@bancoazteca.com>
2021-03-11 18:05:22 -0600

    proyecto limpio

--- BarraBusqueda/ViewController.swift
+++ BarraBusqueda/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  BarraBusqueda
-//
-//  Created by edeluz on 11/03/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
